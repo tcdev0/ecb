4797a69d48514fbdf55a7bbad1d777cadba18375
tcdev <toni.choitz@insanetec.de>
2013-01-30 14:49:15 +0100

    add ExchangeRates Class and implement Service php_mag_ecb.rates

---
+++ src/KIJ/Bundle/EcbBundle/Ecb/ExchangeRates.php
@@ -0,0 +1,46 @@
+<?php
+
+    namespace KIJ\Bundle\EcbBundle\Ecb;
+
+    class ExchangeRates
+    {
+        private $adapter;
+        private $parser;
+        private $data;
+
+        public function __construct(AdapterInterface $adapter, ParserInterface $parser)
+        {
+            $this->adapter = $adapter;
+            $this->parser  = $parser;
+            $this->data    = null;
+        }
+
+        public function getRates()
+        {
+            $this->loadData();
+
+            return $this->data;
+        }
+
+        public function getRate($currency)
+        {
+            $this->loadData();
+
+            if(!isset($this->data[$currency]))
+            {
+                throw new \InvalidArgumentException(sprintf('No data for currency %s', $currency));
+            }
+
+            return $this->data[$currency];
+        }
+
+        private function loadData()
+        {
+            if(null === $this->data)
+            {
+                $xml = $this->adapter->getRawData();
+                $this->data = $this->parser->parse($xml);
+            }
+        }
+
+    }
